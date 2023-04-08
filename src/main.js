9c5f5a37bc6b2ccd6d108c7542742c41560f26d5
thuan237 <congthuan701@gmail.com>
2023-04-08 16:28:57 +0700

    feat: init project

--- src/main.js
+++ src/main.js
@@ -2,4 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+// import tailwind
+import "./assets/style/tailwind.css";
+
+import "./assets/style/global.css";
+
 createApp(App).use(router).mount("#app");
